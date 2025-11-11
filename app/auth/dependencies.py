4c4fe91f206aa717dcaf07a3b4e154d28471d6d3
Maxiel De Jesus <mid6@njit.edu>
2025-11-10 20:09:30 -0500

    added app/auth/dependecies.py that defines auth dependecies for FastAPI.

---
+++ app/auth/dependencies.py
@@ -0,0 +1,42 @@
+# app/auth/dependencies.py
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from app.models.user import User
+from app.schemas.user import UserResponse
+
+# Makes FastAPI know that it is a security scheme. So it is added that way to OpenAPI.
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def get_current_user(
+    db,
+    token: str = Depends(oauth2_scheme) # Extract token which should be user id
+) -> UserResponse:
+    """Dependency to get current user from JWT token."""
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    
+    user_id = User.verify_token(token) # Verify user
+    if user_id is None:
+        raise credentials_exception
+    
+    # Query the user record using Pydeantic schema to convert SQLalchemy
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise credentials_exception
+        
+    return UserResponse.model_validate(user)  # Updated from from_orm
+
+def get_current_active_user(
+    current_user: UserResponse = Depends(get_current_user)
+) -> UserResponse:
+    """Dependency to get current active user."""
+    if not current_user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Inactive user"
+        )
+    return current_user
